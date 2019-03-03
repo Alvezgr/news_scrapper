e599e84bf381605a52954b700cfa46a2c8327349
Gerardo <alvezpope@gmail.com>
2019-03-03 18:12:43 -0300

    Generate uids for rows

--- newspaper_receipe.py
+++ newspaper_receipe.py
@@ -1,3 +1,4 @@
+import hashlib
 import argparse
 import logging
 from urllib.parse import urlparse
@@ -16,7 +17,7 @@ def main(filename):
     df = _add_newspaper_uid_column(df, newspaper_uid)
     df = _extract_host(df)
     df = _fill_nan_data(df)
-
+    df = _generate_uids_for_rows(df)
     return df
 
 def _extract_newspaper_uid(filename):
@@ -54,13 +55,25 @@ def _fill_nan_data(df):
     logger.info('Filling Missing titles')
     missing_titles_mask = df['title'].isna()
 
-    missing_titles = (df[missing_titles_mask]['uri']
-                      .str.extract(r'(?P<missing_titles>[^/]+)$')
-                      .applymap(lambda title: title.split('-'))
-                      .applymap(lambda title_word_list: ' '.join(title_word_list))
-                    )
+    missing_titles = (
+        df[missing_titles_mask]['uri']
+        .str.extract(r'(?P<missing_titles>[^/]+)$')
+        .applymap(lambda title: title.split('-'))
+        .applymap(lambda title_word_list: ' '.join(title_word_list))
+    )
     df.loc[missing_titles_mask, 'title'] = missing_titles.loc[:, 'missing_titles']
     return df
+
+def _generate_uids_for_rows(df):
+    """Generate uids for each article in the dataset"""
+    logger.info('Generating uids for each row')
+    uids = (df
+            .apply(lambda row: hashlib.md5(bytes(row['uri'].encode())), axis=1)
+            .apply(lambda hash_object: hash_object.hexdigest())
+   )
+    df['uid'] = uids
+    return df.set_index('uid')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
